710d81f48e20c158c066ee7e5fa5ecd6c8316e31
Evgenii Komrakov <ekomrak@gmail.com>
2020-01-27 18:08:45 +0300

    Revert "Initial commit"
    This reverts commit 87385c6d5c5e09c194c087d2bb41a5c99405dc64.

--- src/main/java/com/netcracker/dataapp/repositories/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.netcracker.dataapp.repositories;
-
-import com.netcracker.dataapp.entities.Address;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AddressRepository extends CrudRepository<Address, Long> {
-}
